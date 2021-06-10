23f7c9500f7bb64600c44e158af5edbb0f2746ff
sunawachi <natsumi.takahashi.jgc@gmail.com>
2021-06-11 08:51:51 +0900

    for processing DataFrame columns, used apply() instead of the for statement

--- hashing.py
+++ hashing.py
@@ -25,9 +25,9 @@ def main():
         print('No such column in the csv: "%s"' % target_column_name)
         exit()
 
-    hashed_df = hash_column(df, target_column_name)
+    df[target_column_name] = df[target_column_name].apply(hash)
     new_data_name = genelate_new_csv_name(data_name)
-    hashed_df.to_csv(new_data_name, encoding='utf_8_sig')
+    df.to_csv(new_data_name, encoding='utf_8_sig')
     print('The process has been completed.')
 
 def let_input_string(message):
@@ -37,13 +37,11 @@ def let_input_string(message):
         print('could not convert the input to a string')
         exit()
 
-def hash_column(dataframe, columnname):
-    for i in range(len(dataframe.index)):
-        m = hashlib.sha256()
-        b = bytes(str(dataframe.loc[dataframe.index[i], columnname]), encoding='utf8')
-        m.update(b)
-        dataframe.loc[dataframe.index[i], columnname] = m.digest()
-    return dataframe
+def hash(val):
+    m = hashlib.sha256()
+    b = bytes(str(val), encoding='utf8')
+    m.update(b)
+    return m.digest()
 
 def genelate_new_csv_name(filename):
     newname = filename[:-4] + "_hashed.csv"
